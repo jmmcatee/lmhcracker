cf72d28478855f0391db1327062398163696567f
Michael McAtee <jmmcatee@gmail.com>
2012-04-02 23:15:39 -0400

    Updated to Go version 1!
    Added the following changes to make lmhcracker Go1 functional
    - Changed all Time types to work with the new Time package specs (read Durations and Formats)
    - Ran go fix to adjust imports and other code cleanups
    - Added -p default to be the max number of cores
    - Removed the Makefile that is not needed with "go build"
    - Moved source to root with README
    - Updated README to reflect "go build" and new defaults for number of cores

--- README.md
+++ README.md
@@ -4,22 +4,15 @@ lmhcracker is a tool to generate and brute force crack Windows LM Hashes. It use
 
 ## Installation
 
-lmhcracker currently works with Go release r60.
+lmhcracker currently works with Go version 1.
 
-**Linux**
-
-	$ gomake
-	$ ./lmhcracker {flags} PARAMS
-
-**Windows**
-
-	$ C:\Go\bin\6g.exe lmhcracker.go
-	$ C:\Go\bin\6l.exe -o .\lmhcracker.exe lmhcracker.6
+	$ go build lmhcracker.go
+	$ ./lmhcracker {flags} LMHash LMHash ...
 
 ## Running
-To run lmhcracker you can just hand it a full LM Hash. You can set the number of routines (threads) used with the -p flag (default is 2).
+To run lmhcracker you can just hand it a full LM Hash. You can set the number of routines (threads) used with the -p flag (default is max number of CPU cores).
 
 ## Example
-The following command attempts to brute force LM Hashes (8 byte halves) for BLANK, ABCD, & 1234 using 4 guessing processes (Best to set -p to the number of cores or threads you have if that is more than two or leave it at the default, which is two).
+The following command attempts to brute force LM Hashes (8 byte halves) for blank, ABCD, & 1234 using the maximum number of available cores.
 
-	./lmhcracker -p 4 e165f0192ef85ebbaad3b435b51404ee b757bf5c0d87772faad3b435b51404ee
+	./lmhcracker e165f0192ef85ebbaad3b435b51404ee b757bf5c0d87772faad3b435b51404ee
--- lmhcracker.go
+++ lmhcracker.go
@@ -2,65 +2,62 @@ package main
 
 import (
 	"bytes"
-	"encoding/hex"
-	"fmt"
-	"flag"
 	"crypto/des"
-	"runtime"
-	"time"
-	"os"
-	"runtime/pprof"
+	"encoding/hex"
+	"flag"
+	"fmt"
 	"log"
+	"os"
+	"runtime"
+	"runtime/pprof"
+	"time"
 )
 
-
-var numParallelOp = flag.Int("p", 2, "Number of parallel processes to run")
+var numParallelOp = flag.Int("p", runtime.NumCPU(), "Number of parallel processes to run")
 var cpuprofile = flag.String("cpuprofile", "", "Write cpu profile to file")
 var runBenchmark = flag.Bool("b", false, "Run program brenchmark")
 
 var lmhashConstant = []byte("KGS!@#$%")
-var posChars = []byte{0x00 /* NULL  */,
-	0x41 /* A */, 0x42 /* B */, 0x43 /* C */, 0x44 /* D */, 0x45 /* E */,
-	0x46 /* F */, 0x47 /* G */, 0x48 /* H */, 0x49 /* I */, 0x4A /* J */,
-	0x4B /* K */, 0x4C /* L */, 0x4D /* M */, 0x4E /* N */, 0x4F /* O */,
-	0x50 /* P */, 0x51 /* Q */, 0x52 /* R */, 0x53 /* S */, 0x54 /* T */,
-	0x55 /* U */, 0x56 /* V */, 0x57 /* W */, 0x58 /* X */, 0x59 /* Y */,
-	0x5A /* Z */, 0x30 /* 0 */, 0x31 /* 1 */, 0x32 /* 2 */, 0x33 /* 3 */,
-	0x34 /* 4 */, 0x35 /* 5 */, 0x36 /* 6 */, 0x37 /* 7 */, 0x38 /* 8 */,
-	0x39 /* 9 */, 0x21 /* ! */, 0x3F /* ? */, 0x2C /* , */, 0x2D /* - */,
-	0x2E /* . */, 0x40 /* @ */, 0x20 /*   */, 0x22 /* " */, 0x23 /* # */,
-	0x24 /* $ */, 0x25 /* % */, 0x26 /* & */, 0x27 /* ' */, 0x28 /* ( */,
-	0x29 /* ) */, 0x2A /* * */, 0x2B /* + */, 0x2F /* / */, 0x3A /* : */,
-	0x3B /* ; */, 0x3C /* < */, 0x3D /* = */, 0x3E /* > */, 0x5B /* [ */,
-	0x5C /* \ */, 0x5D /* ] */, 0x5E /* ^ */, 0x5F /* _ */, 0x60 /* ` */,
+var posChars = []byte{0x00, /* NULL  */
+	0x41 /* A */, 0x42 /* B */, 0x43 /* C */, 0x44 /* D */, 0x45, /* E */
+	0x46 /* F */, 0x47 /* G */, 0x48 /* H */, 0x49 /* I */, 0x4A, /* J */
+	0x4B /* K */, 0x4C /* L */, 0x4D /* M */, 0x4E /* N */, 0x4F, /* O */
+	0x50 /* P */, 0x51 /* Q */, 0x52 /* R */, 0x53 /* S */, 0x54, /* T */
+	0x55 /* U */, 0x56 /* V */, 0x57 /* W */, 0x58 /* X */, 0x59, /* Y */
+	0x5A /* Z */, 0x30 /* 0 */, 0x31 /* 1 */, 0x32 /* 2 */, 0x33, /* 3 */
+	0x34 /* 4 */, 0x35 /* 5 */, 0x36 /* 6 */, 0x37 /* 7 */, 0x38, /* 8 */
+	0x39 /* 9 */, 0x21 /* ! */, 0x3F /* ? */, 0x2C /* , */, 0x2D, /* - */
+	0x2E /* . */, 0x40 /* @ */, 0x20 /*   */, 0x22 /* " */, 0x23, /* # */
+	0x24 /* $ */, 0x25 /* % */, 0x26 /* & */, 0x27 /* ' */, 0x28, /* ( */
+	0x29 /* ) */, 0x2A /* * */, 0x2B /* + */, 0x2F /* / */, 0x3A, /* : */
+	0x3B /* ; */, 0x3C /* < */, 0x3D /* = */, 0x3E /* > */, 0x5B, /* [ */
+	0x5C /* \ */, 0x5D /* ] */, 0x5E /* ^ */, 0x5F /* _ */, 0x60, /* ` */
 	0x7B /* { */, 0x7C /* | */, 0x7D /* } */, 0x7E /* ~ */}
 var benchmarkInput = make([][]byte, 1)
 
 type guessStatus struct {
-	PosID     int
-	Guesses   int64
-	RunTime	  int64
-	FoundHash bool
-	Guess     []byte
+	PosID            int
+	Guesses          int64
+	RunTime          float64
+	FoundHash        bool
+	Guess            []byte
 	HashCrackedIndex int
 }
 
 type guessInfo struct {
-        Position int
-        PosID int
-        GuessRange []byte
-        KnownHashes [][]byte
-        Ch chan guessStatus
-        CurrentSeed []byte
-        GuessStartTime *time.Time
-        Guesses int64
+	Position       int
+	PosID          int
+	GuessRange     []byte
+	KnownHashes    [][]byte
+	Ch             chan guessStatus
+	CurrentSeed    []byte
+	GuessStartTime time.Time
+	Guesses        int64
 }
-
-
 
 func main() {
 	// Start program timer
-	progStartTime := time.LocalTime()
+	progStartTime := time.Now()
 
 	// Parse flag inputs
 	flag.Parse()
@@ -76,34 +73,28 @@ func main() {
 	}
 
 	// Set the number of threads using the input parameter given
-	runtime.GOMAXPROCS(*numParallelOp+1)
+	runtime.GOMAXPROCS(*numParallelOp + 1)
 
 	var arraySize = 0
 	if *runBenchmark {
 		// ABCD
 		benchmarkInput[0] = []byte{0xE1, 0x65, 0xF0, 0x19, 0x2E, 0xF8,
-									0x5E, 0xBB, 0xAA, 0xD3, 0xB4, 0x35, 
-									0xB5, 0x14, 0x04, 0xEE}
+			0x5E, 0xBB, 0xAA, 0xD3, 0xB4, 0x35, 0xB5, 0x14, 0x04, 0xEE}
 		// 1234
 		benchmarkInput = append(benchmarkInput, []byte{0xB7, 0x57, 0xBF, 0x5C, 0x0D, 0x87,
-														0x77, 0x2F, 0xAA, 0xD3, 0xB4, 0x35,
-														0xB5, 0x14, 0x04, 0xEE})
+			0x77, 0x2F, 0xAA, 0xD3, 0xB4, 0x35, 0xB5, 0x14, 0x04, 0xEE})
 		// 9999
 		benchmarkInput = append(benchmarkInput, []byte{0xA5, 0x93, 0x8F, 0x23, 0xD5, 0x47,
-														0x19, 0xA0, 0xAA, 0xD3, 0xB4, 0x35,
-														0xB5, 0x14, 0x04, 0xEE})
+			0x19, 0xA0, 0xAA, 0xD3, 0xB4, 0x35, 0xB5, 0x14, 0x04, 0xEE})
 		// AAAAA
 		benchmarkInput = append(benchmarkInput, []byte{0xA8, 0x0F, 0x6E, 0x6A, 0x87, 0xBA,
-														0x6A, 0xC2, 0xAA, 0xD3, 0xB4, 0x35,
-														0xB5, 0x14, 0x04, 0xEE})
+			0x6A, 0xC2, 0xAA, 0xD3, 0xB4, 0x35, 0xB5, 0x14, 0x04, 0xEE})
 		// EDCBA
 		benchmarkInput = append(benchmarkInput, []byte{0x40, 0x97, 0xB2, 0x61, 0xB6, 0xA1,
-														0x34, 0xCA, 0xAA, 0xD3, 0xB4, 0x35,
-														0xB5, 0x14, 0x04, 0xEE})
+			0x34, 0xCA, 0xAA, 0xD3, 0xB4, 0x35, 0xB5, 0x14, 0x04, 0xEE})
 		// 1234F
 		benchmarkInput = append(benchmarkInput, []byte{0x0E, 0xBB, 0x05, 0xDE, 0xE2, 0xD1,
-															0xc0, 0xb9, 0xAA, 0xD3, 0xB4, 0x35,
-															0xB5, 0x14, 0x04, 0xEE})
+			0xc0, 0xb9, 0xAA, 0xD3, 0xB4, 0x35, 0xB5, 0x14, 0x04, 0xEE})
 
 		arraySize = len(benchmarkInput) * 2
 	} else {
@@ -151,7 +142,7 @@ func main() {
 				foundIndex = j
 			}
 		}
-		
+
 		if !check {
 			if i < 2 {
 				unKnownLMHashes[i] = input
@@ -175,14 +166,14 @@ func main() {
 	guessSplit := divideWork()
 
 	for i, value := range guessSplit {
-		params := guessInfo{	6,
-								i,
-								value,
-								unKnownLMHashes,
-								statusCh,
-								[]byte{value[0],0x00,0x00,0x00,0x00,0x00,0x00,0x00},
-								time.LocalTime(),
-								0													}
+		params := guessInfo{6,
+			i,
+			value,
+			unKnownLMHashes,
+			statusCh,
+			[]byte{value[0], 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+			time.Now(),
+			0}
 		go guessHashes(&params)
 	}
 
@@ -193,9 +184,9 @@ func main() {
 			crackedPassword[stat.HashCrackedIndex] = stat.Guess
 			foundKeys++
 		} else {
-			if stat.RunTime!=0{
-				fmt.Printf("- Process %d: Guesses:%d (%d/s) Last Guess:%X\n", stat.PosID,
-				stat.Guesses, stat.Guesses/(stat.RunTime), stat.Guess)
+			if stat.RunTime != 0 {
+				fmt.Printf("- Process %d: Guesses:%d (%f/s) Last Guess:%X\n", stat.PosID,
+					stat.Guesses, float64(stat.Guesses)/stat.RunTime, stat.Guess)
 			}
 		}
 
@@ -205,13 +196,13 @@ func main() {
 	}
 
 	time.Sleep(10)
-	fmt.Printf("\n\nFound Keys (Took %d seconds)\n", time.Seconds()-progStartTime.Seconds())
+	fmt.Printf("\n\nFound Keys (Took %s seconds)\n", time.Now().Sub(progStartTime))
 
 	// Print hashes in correct order with inputToCrackedMap
 	for i, _ := range inputHashes {
-		if i % 2 == 1 && i != 0 {
+		if i%2 == 1 && i != 0 {
 			fmt.Printf("- Given: %X%X \n", inputHashes[i-1], inputHashes[i])
-			_first  := crackedPassword[inputToCrackedMap[i-1]]
+			_first := crackedPassword[inputToCrackedMap[i-1]]
 			_second := crackedPassword[inputToCrackedMap[i]]
 			fmt.Printf("  Cracked: %X%X (%s%s)\n", _first, _second, _first, _second)
 		}
@@ -223,8 +214,12 @@ func divideWork() [][]byte {
 	guessSplit := make([][]byte, *numParallelOp)
 	j := 0
 	for _, value := range posChars {
-		guessSplit[j]  = append(guessSplit[j], value)
-		if j == (*numParallelOp-1) { j=0 } else { j++ }
+		guessSplit[j] = append(guessSplit[j], value)
+		if j == (*numParallelOp - 1) {
+			j = 0
+		} else {
+			j++
+		}
 	}
 	return guessSplit
 }
@@ -233,16 +228,16 @@ func guessHashes(params *guessInfo) (paramsOut *guessInfo) {
 	for _, currentChar := range posChars {
 		params.CurrentSeed[params.Position] = currentChar
 		if params.Position > 0 {
-			params.Position = params.Position-1
+			params.Position = params.Position - 1
 			params = guessHashes(params)
 		}
 		if params.Position == 4 {
-			params.Ch <- guessStatus{	params.PosID,
-										params.Guesses,
-										time.Seconds()-params.GuessStartTime.Seconds(),
-										false,
-										params.CurrentSeed,
-										0												}
+			params.Ch <- guessStatus{params.PosID,
+				params.Guesses,
+				time.Now().Sub(params.GuessStartTime).Seconds(),
+				false,
+				params.CurrentSeed,
+				0}
 		}
 
 		if params.Position == 0 {
@@ -250,33 +245,33 @@ func guessHashes(params *guessInfo) (paramsOut *guessInfo) {
 				params.CurrentSeed[params.Position] = currentGuess
 				var guessHash = createLMHash(params.CurrentSeed)
 				for i, currentCheckHash := range params.KnownHashes {
-					if bytes.Compare(guessHash, currentCheckHash)==0 {
+					if bytes.Compare(guessHash, currentCheckHash) == 0 {
 						// The []byte call keeps the data in the channel from being
 						// changed once it is mapped on the other side. By 
 						// creating a seperate array a new untouched space is used.
-						params.Ch <- guessStatus{	params.PosID,
-													params.Guesses,
-													time.Seconds()-params.GuessStartTime.Seconds(),
-													true,
-													[]byte{	params.CurrentSeed[0],
-															params.CurrentSeed[1],
-															params.CurrentSeed[2],
-															params.CurrentSeed[3],
-															params.CurrentSeed[4],
-															params.CurrentSeed[5],
-															params.CurrentSeed[6],
-															0x00					},
-													i												}
-						fmt.Printf("::FOUND KEY: %s (%X) at (%d seconds) process:%d\n",
+						params.Ch <- guessStatus{params.PosID,
+							params.Guesses,
+							time.Now().Sub(params.GuessStartTime).Seconds(),
+							true,
+							[]byte{params.CurrentSeed[0],
+								params.CurrentSeed[1],
+								params.CurrentSeed[2],
+								params.CurrentSeed[3],
+								params.CurrentSeed[4],
+								params.CurrentSeed[5],
+								params.CurrentSeed[6],
+								0x00},
+							i}
+						fmt.Printf("::FOUND KEY: %s (%X) at (%s seconds) process:%d\n",
 							params.CurrentSeed, params.CurrentSeed,
-							time.Seconds()-params.GuessStartTime.Seconds(), params.PosID)
+							time.Now().Sub(params.GuessStartTime), params.PosID)
 					}
 				}
 				params.Guesses++
 			}
 			/* Because the 0 index has split guess characters for parallel work break once loop 
-				is done to allow the 1 index to increment to the next full posChars character.
-			 */
+			is done to allow the 1 index to increment to the next full posChars character.
+			*/
 			break
 		}
 	}
@@ -287,18 +282,18 @@ func guessHashes(params *guessInfo) (paramsOut *guessInfo) {
 func createLMHash(passHalve []byte) (hashHalve []byte) {
 	// Create 0'd key and hash slices and set the constant input
 	// used to the password halve key against.
-	key  := make([]byte, 8)
+	key := make([]byte, 8)
 	hash := make([]byte, 8)
 
 	// Do bit flipping to turn password halve into a DES key
-	key[0] = (passHalve[0]>>1)<<1
-	key[1] = ( ((passHalve[0]&0x01)<<6) | (passHalve[1]>>2) )<<1
-        key[2] = ( ((passHalve[1]&0x03)<<5) | (passHalve[2]>>3) )<<1
-        key[3] = ( ((passHalve[2]&0x07)<<4) | (passHalve[3]>>4) )<<1
-        key[4] = ( ((passHalve[3]&0x0F)<<3) | (passHalve[4]>>5) )<<1
-        key[5] = ( ((passHalve[4]&0x1F)<<2) | (passHalve[5]>>6) )<<1
-	key[6] = ( ((passHalve[5]&0x3F)<<1) | (passHalve[6]>>7) )<<1
-        key[7] = (passHalve[6]&0x7F)<<1
+	key[0] = (passHalve[0] >> 1) << 1
+	key[1] = (((passHalve[0] & 0x01) << 6) | (passHalve[1] >> 2)) << 1
+	key[2] = (((passHalve[1] & 0x03) << 5) | (passHalve[2] >> 3)) << 1
+	key[3] = (((passHalve[2] & 0x07) << 4) | (passHalve[3] >> 4)) << 1
+	key[4] = (((passHalve[3] & 0x0F) << 3) | (passHalve[4] >> 5)) << 1
+	key[5] = (((passHalve[4] & 0x1F) << 2) | (passHalve[5] >> 6)) << 1
+	key[6] = (((passHalve[5] & 0x3F) << 1) | (passHalve[6] >> 7)) << 1
+	key[7] = (passHalve[6] & 0x7F) << 1
 
 	// Create a new DES cipher and then encrypt the LM constant
 	// with the DES key made from the password halve provided
